fc35baafdaa416bb257d3a3092211c8893c1abf2
KasunKVI <kkhettiarachchi10@gmail.com>
2023-10-22 10:49:39 +0530

    complete the all forms

--- assets/js/script.js
+++ assets/js/script.js
@@ -23,3 +23,30 @@ $("#nav-icon-close").on("click", () => {
         $("#navbarMob").css("transform", "translateX(20px)");
     }, 200);
 });
+$("#nav-icon-setting").on("click", () => {
+
+    $("#account_details").css("display", "block");
+    $("#nav-icon-setting").css("display" , "none");
+    $("#nav-icon-setting-close").css("display" , "block");
+
+    // Apply the transitioned styles
+    setTimeout(() => {
+        $("#account_details").css("opacity", "1");
+        $("#nav-icon-setting-close").css("opacity" , "1");
+        $("#account_details").css("transform", "translateY(33vh");
+    }, 200);
+});
+
+$("#nav-icon-setting-close").on("click", () => {
+
+    // $("#account_details").css("display", "none");
+    $("#nav-icon-setting-close").css("display" , "none");
+    $("#nav-icon-setting").css("display" , "block");
+
+    // Apply the transitioned styles
+    setTimeout(() => {
+        $("#account_details").css("opacity", "0");
+        $("#nav-icon-setting-close").css("opacity" , "0");
+        $("#account_details").css("transform", "translateY(-40vh");
+    }, 200);
+});
